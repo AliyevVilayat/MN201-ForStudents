4cb9f32ab758d34fa5a8bb071ee1cf794e0d0951
AliyevVilayat <77es4pa@code.edu.az>
2024-01-11 12:58:46 +0400

    has been added lab 01-10-2024

---
+++ Lab_10_01_2024/Lab_W_Students_01_10_2024/LabReflection/Program.cs
@@ -0,0 +1,54 @@
+﻿
+using System.Reflection;
+
+string path = @"C:\Users\Asus\source\repos\LabReflection\LibraryForReflection\bin\Debug\net6.0\LibraryForReflection.dll";
+Assembly asmb = Assembly.LoadFile(path);
+
+
+Type? type = asmb.GetType("LibraryForReflection.TestClassForReflection");
+
+
+if (type is not null)
+{
+    var obj = Activator.CreateInstance(type);
+
+    /*MethodInfo? methodInfo = type.GetMethod("TestMethod",BindingFlags.Public|BindingFlags.NonPublic|BindingFlags.Instance |BindingFlags.Static);
+
+    if(methodInfo is not null)
+    {
+        methodInfo.Invoke(obj,null);
+    }*/
+
+    /*PropertyInfo[] propertyInfos = type.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+
+    if (propertyInfos.Length > 0)
+    {
+        foreach (PropertyInfo propertyInfo in propertyInfos)
+        {
+
+            Console.WriteLine($"{propertyInfo.Name}'value: {propertyInfo.GetValue(obj)}"); 
+        }
+    }*/
+
+    FieldInfo[] fieldInfos = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+
+    if (fieldInfos.Length > 0)
+    {
+        foreach (FieldInfo fieldInfo in fieldInfos)
+        {
+
+            Console.WriteLine($"{fieldInfo.Name}'value: {fieldInfo.GetValue(obj)}");
+        }
+    }
+
+}
+
+/*foreach (Type type in asmb.GetTypes())
+{
+    MethodInfo[] methodInfo = type.GetMethods();
+
+    foreach(MethodInfo method in methodInfo)
+    {
+        Console.WriteLine(method.Name);
+    }
+}*/
---
+++ Lab_10_01_2024/Lab_W_Students_01_10_2024/LibraryForReflection/TestClassForReflection.cs
@@ -0,0 +1,12 @@
+﻿namespace LibraryForReflection;
+
+class TestClassForReflection
+{
+    private int _id =1;
+    private string Name { get; set; } = "TestName";
+
+    private void TestMethod()
+    {
+        Console.WriteLine("Everything is lie");
+    }
+}
